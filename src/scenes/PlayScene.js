ab8ca1e48c6d0bf777cff602fd02949dc213ae44
roma_marshall <roma_marshall@outlook.com>
2024-09-01 15:30:47 +0200

    display fixes

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -176,13 +176,13 @@ class PlayScene extends BaseScene {
   createScore() {
     this.score = 0
     const bestScore = localStorage.getItem('bestScore')
-    this.scoreText = this.add.text(16, 56, `Score: ${0}`, {fontSize: '32px', color: '#fff'})
-    this.add.text(16, 92, `Best score: ${bestScore || 0}`, {fontSize: '18px', color: '#fff'})
+    this.scoreText = this.add.text(16, 66, `Score: ${0}`, {fontSize: '32px', color: '#fff'})
+    this.add.text(16, 102, `Best score: ${bestScore || 0}`, {fontSize: '18px', color: '#fff'})
   }
 
   createPause() {
     this.isPaused = false
-    const pauseButton = this.add.image(this.config.width - 50,this.config.height - 50,'pause')
+    const pauseButton = this.add.image(this.config.width - 30,this.config.height - 70,'pause')
       .setInteractive()
       .setScale(3)
       .setOrigin(1)
