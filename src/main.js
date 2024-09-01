107643bee5c05f7567340be57712de1d79e02ee4
roma_marshall <roma_marshall@outlook.com>
2024-09-01 14:15:11 +0200

    edited resolution for mobile devices

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import PreloadScene from './scenes/PreloadScene'
 import ScoreScene from './scenes/ScoreScene'
 import PauseScene from './scenes/PauseScene'
 
-const WIDTH = 390
+const WIDTH = 450
 const HEIGHT = 844
 const BIRD_POSITION = {
     x: WIDTH / 10,
